890bc143f38b60f907461d3e73557933099af748
Cale Ryden <cale.ryden@edu.sait.ca>
2025-12-14 00:20:47 -0700

    Added pretty much all UI for search, results and dashboard

--- app/Search/page.js
+++ app/Search/page.js
@@ -1,21 +1,33 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 
 export default function Search() {
-    return (
+     const [query, setQuery] = useState("");
+    const router = useRouter();
+
+    const handleSearch = () => {
+        if (!query.trim()) return;
+
+    router.push(`/results?movie=${encodeURIComponent(query)}`);
+  };
+
+    return (    
         <main>
             <div className="flex-col flex h-screen justify-center items-center">
                 <div className="flex fixed top-7 text-white w-70 text-2xl justify-center items-center mb-15">
                     <div>
-                    <h1 className="rainbow-text text-[70px]">NCR Movie Watchlist</h1> 
+                    <h1 className="rainbow-text text-[70px] underline decoration-white">NCR Movie Watchlist</h1> 
                     </div>
                 </div>
                 
                 <div>
                     <p className="rainbow-text text-[40px]">THIS IS THE SICK ASS SEARCH BAR (Sharknado Only.)</p>
-                    <div className="mt-5 text-center">
-                        <input type="text" placeholder="Enter Movie Title..." className="border-2 rounded-md pl-2 w-64 rainbow-text text-[30px]"></input>
-                        <button className="border-2 rounded-md pl-2 pr-2 text-center w-24 rainbow-text text-[30px]">Search</button>
+                    <div className="mt-5 text-center flex gap-3">
+                        <input type="text" placeholder="Enter Movie Title..." value={query} onChange={(e) => setQuery(e.target.value)} className="border-2 rounded-md pl-2 w-64 rainbow-text text-[30px]"></input>
+                        <button onClick={handleSearch} className="border-2 rounded-md pl-2 pr-2 text-center w-24 rainbow-text text-[30px] cursor-pointer">Search</button>
                     </div>
                 </div>
 
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { auth } from "../lib/firebase";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -15,8 +16,19 @@ export default function Dashboard() {
 
   return (
     <main style={{ padding: "40px" }}>
-      <h1>Dashboard</h1>
-      <p>You are logged in 🎉</p>
+      <div className="flex mt-4 text-[50px]">
+          <Link href="/search" className="flex border-2 rounded-md w-50 h-15 m-auto items-center justify-center rainbow-text fixed top-4 right-4">Go To Search</Link>
+        </div>
+      <div className="text-center items-center justify-center rainbow-text text-[50px]">
+        <h1 className="rainbow-text text-[70px] underline decoration-white">NCR Movie Watchlist</h1> 
+      </div>
+      <div className="flex flex-col items-center justify-center mt-10 gap-5">
+        <p className="rainbow-text text-[50px]">Current Watchlist</p>
+        <p className="flex border-2 w-350 h-50"></p>
+        
+        <p className="rainbow-text text-[50px]">Movies you've watched</p>
+        <p className="flex border-2 w-350 h-50"></p>
+      </div>
     </main>
   );
 }
